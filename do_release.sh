623bd0b7b5fa81a2049a1233bab9a1bad1e00cbe
masa213f <masa213f@gmail.com>
2019-09-27 15:52:27 +0000

    Change sample tag prefix

--- do_release.sh
+++ do_release.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-TAG=release-v0.1.0         # official release version
-TAG_RC=release-v0.1.0-rc.0 # pre-release version
-TAG_NG=release-v0.1.0.0    # invalid versioning format
+TAG_NG=v0.1.0.invalid   # invalid versioning format
+TAG_RC=v0.1.0-rc.0      # pre-release version
+TAG_OK=v0.1.0           # official release version
 
 if [ "x$1" == "xdelete" ]; then
   # Delete tags
-  git tag -d ${TAG}
-  git tag -d ${TAG_RC}
   git tag -d ${TAG_NG}
-  git push origin :${TAG}
-  git push origin :${TAG_RC}
   git push origin :${TAG_NG}
+
+  git tag -d ${TAG_RC}
+  git push origin :${TAG_RC}
+
+  git tag -d ${TAG_OK}
+  git push origin :${TAG_OK}
   exit
 fi
 
 # Create tags
-git tag ${TAG}
-git tag ${TAG_RC}
 git tag ${TAG_NG}
-git push origin ${TAG} -f
-git push origin ${TAG_RC} -f
 git push origin ${TAG_NG} -f
+
+git tag ${TAG_RC}
+git push origin ${TAG_RC} -f
+
+git tag ${TAG_OK}
+git push origin ${TAG_OK} -f
